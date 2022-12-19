48a4a572f7cfe05f600b47dfb4b734b1bc74f75b
AleLanzi <119108870+AleLanzi@users.noreply.github.com>
2022-12-19 16:38:24 +0100

    Add files via upload

---
+++ VariablesandTemplates2/src/main.kt
@@ -0,0 +1,12 @@
+
+
+fun main(){
+    val number1 = 3
+    val number2 : Int = 66
+    val number3 : Float = 2000f
+
+    val sum = number1 + number2 + number3
+
+    println("The sum is: ${sum}")
+
+}
