5e3885655dfe0de43a4df5fa94143e6c662ac8c4
AleLanzi <119108870+AleLanzi@users.noreply.github.com>
2022-12-19 16:28:37 +0100

    Add files via upload

---
+++ VariablesAndStringTemplates/src/it/develhope/main.kt
@@ -0,0 +1,10 @@
+package it.develhope
+
+fun main(){
+
+    val number1 = 456
+    val name = "Alessio"
+    val surname = "lanzi"
+    println("The first variable is a number: ${number1}, the second variable is a string: ${name}, the third variable is also a string: ${surname}")
+
+}
