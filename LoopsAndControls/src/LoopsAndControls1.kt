7c2bd13352b550c4f580cedd9b1c232e49565360
AleLanzi <119108870+AleLanzi@users.noreply.github.com>
2022-12-20 17:26:30 +0100

    Add files via upload

---
+++ LoopsAndControls/src/LoopsAndControls1.kt
@@ -0,0 +1,11 @@
+
+fun main(){
+
+    for(i in 1..100 step 2){
+        if(i<50)
+            println("In the first half we got: $i")
+         else
+            println("In the second half we got: $i")
+
+    }
+}
